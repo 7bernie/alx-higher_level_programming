74f3f12d7f0bac99aef1a2026be3e573c5d9fb99
7bernie <bernardbassie@gmail.com>
2023-09-09 12:31:30 -0700

    Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,27 +1,60 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <Python.h>
+#include <python.h>
 
 /**
- * print_python_list_info - prints python list info
+ * print_python_list_info - print info about a Python list.
+ * @p: pointer to a list object
  *
- * @p: PyObject
- * Return: no return
+ * Return: Nothing.
  */
 void print_python_list_info(PyObject *p)
 {
-	long int size, i;
-	PyListObject *list;
-	PyObject *item;
+Py_ssize_t idx = 0;
+PyObject *item = NULL;
 
-	size = Py_SIZE(p);
-	printf("[*] Size of the Python List = %ld\n", size);
+printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+for (; idx < PyList_Size(p); idx++)
+{
+	item = PyList_GetItem(p, idx);
+	printf("Element %ld: %s\n", idx, item->ob_type->tp_name);
+}
+}
 
-	list = (PyListObject *)p;
-	printf("[*] Allocated = %ld\n", list->allocated);
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, 1 on failure.
+ */
+int main(void)
+{
+Py_Initialize();
+/* Example list creation */
+PyObject *pList = PyList_New(3);
+PyObject *pValue;
 
-	for (i = 0; i < size; i++)
-	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-	}
+if (pList == NULL)
+{
+	PyErr_Print();
+	return (1);
+}
+
+/* Append objects to the list */
+pValue = PyLong_FromLong(1);
+PyList_SetItem(pList, 0, pValue);
+
+pValue = PyUnicode_DecodeUTF8("Hello", 5, NULL);
+PyList_SetItem(pList, 1, pValue);
+
+pValue = PyFloat_FromDouble(3.14);
+PyList_SetItem(pList, 2, pValue);
+
+/* Print list information */
+print_python_list_info(pList);
+
+/* Clean up and finalize Python */
+Py_DECREF(pList);
+Py_Finalize();
+return (0);
 }
