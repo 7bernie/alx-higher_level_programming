9ec31de019c13de739d3c8cc1bdb1c6b7eb2d58e
7bernie <bernardbassie@gmail.com>
2023-09-09 20:09:10 +0000

    Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,83 +1,27 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-#include <stdlib.h>
 #include <stdio.h>
-#include <python.h>
-=======
-nclude <stdio.h>
->>>>>>> 718241025844ac5b92c9939aac2f9c8f84075ee8
 #include <Python.h>
->>>>>>> f32d3b9cc935d328965e2ea8a14dc147979603e5
 
-<<<<<<< HEAD
-void print_python_list_info(PyObject *p) {
-    Py_ssize_t size, i;
-    PyObject *item;
-
-    size = PyList_Size(p);
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-
-    for (i = 0; i < size; i++) {
-        item = PyList_GetItem(p, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    }
-=======
 /**
- * print_python_list_info - print info about a Python list.
- * @p: pointer to a list object
+ * print_python_list_info - prints python list info
  *
- * Return: Nothing.
+ * @p: PyObject
+ * Return: no return
  */
 void print_python_list_info(PyObject *p)
 {
-Py_ssize_t idx = 0;
-PyObject *item = NULL;
+	long int size, i;
+	PyListObject *list;
+	PyObject *item;
 
-printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
-printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-for (; idx < PyList_Size(p); idx++)
-{
-	item = PyList_GetItem(p, idx);
-	printf("Element %ld: %s\n", idx, item->ob_type->tp_name);
-}
-}
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %ld\n", size);
 
-/**
- * main - Entry point
- *
- * Return: 0 on success, 1 on failure.
- */
-int main(void)
-{
-Py_Initialize();
-/* Example list creation */
-PyObject *pList = PyList_New(3);
-PyObject *pValue;
+	list = (PyListObject *)p;
+	printf("[*] Allocated = %ld\n", list->allocated);
 
-if (pList == NULL)
-{
-	PyErr_Print();
-	return (1);
-}
-
-/* Append objects to the list */
-pValue = PyLong_FromLong(1);
-PyList_SetItem(pList, 0, pValue);
-
-pValue = PyUnicode_DecodeUTF8("Hello", 5, NULL);
-PyList_SetItem(pList, 1, pValue);
-
-pValue = PyFloat_FromDouble(3.14);
-PyList_SetItem(pList, 2, pValue);
-
-/* Print list information */
-print_python_list_info(pList);
-
-/* Clean up and finalize Python */
-Py_DECREF(pList);
-Py_Finalize();
-return (0);
->>>>>>> 718241025844ac5b92c9939aac2f9c8f84075ee8
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
