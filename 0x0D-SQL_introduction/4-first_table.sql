19e4acb4c22ed59e70adc81bae3ed81c4ac2f76d
7bernie <bernardbassie@gmail.com>
2023-11-14 13:56:59 -0800

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- This script creates a table called first_table
+-- with values id and name in current  database
+CREATE TABLE IF NOT EXISTS first_table (
+       id INT,
+       name VARCHAR(256)
+);
